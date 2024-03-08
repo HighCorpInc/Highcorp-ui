a768865d99c5d23b25b7d51fa30ff93a533de429
EtienneLicheron <etienne.licheron@epitech.eu>
2024-03-08 19:27:46 +0000

    workflows

---
+++ README.md
@@ -0,0 +1,1 @@
+# Highcorp-ui
---
+++ src/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Button.scss";
+
+export interface ButtonProps extends React.ComponentProps<"button"> { kind?: "primary" | "secondary"; }
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ kind = "primary", ...props }, ref) => {
+    return <button data-button={`kind:${kind}`} ref={ref} {...props} />;
+  },
+);
