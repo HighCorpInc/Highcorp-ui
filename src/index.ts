4da2823e8da78153f8c3a77450402d3f89f55464
EtienneLicheron <etienne.licheron@epitech.eu>
2024-03-20 20:54:59 +0000

    remove interface exports

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,7 @@
 export { RadialMenu } from "./RadialMenu/RadialMenu";
 export { RadialMenuItem } from "./RadialMenu/RadialMenuItem/RadialMenuItem";
 
-export { RadialMenuProps } from "./RadialMenu/RadialMenuProps.interface";
-export { RadialMenuItemInterface } from "./RadialMenu/RadialMenuItem/RadialMenuItem.interface";
-
 // Icons
-export { IconProps } from "./Icons/iconProps.interface";
-
 export { CarSeatIcon } from "./Icons/CarSeat";
 export { LightningIcon } from "./Icons/Lightning";
 export { SeatBeltIcon } from "./Icons/SeatBelt";
