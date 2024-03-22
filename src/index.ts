930740bac71e30324a75d7f2d20e4941a654f722
Benjamin Vic <contact@benjaminvic.fr>
2024-03-22 16:26:58 +0000

    export dnd

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,8 @@ export { CarSeatIcon } from "./Icons/CarSeat";
 export { LightningIcon } from "./Icons/Lightning";
 export { SeatBeltIcon } from "./Icons/SeatBelt";
 export { TrunkIcon } from "./Icons/Trunk";
+
+// DnD
+
+export { Droppable } from "./Dnd/Droppable";
+export { Draggable } from "./Dnd/Draggable";
